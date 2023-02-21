7fc52282a8bfd5b3b7c29359e19e54e713153a3a
Дмитро Миронов <hattabith@gmail.com>
2023-02-22 00:13:15 +0200

    Test class added

---
+++ VacuumLoggerVIT2/VacuumLoggerVIT2/VacuumLogger.Application/Interfaces/IParse.cs
@@ -0,0 +1,14 @@
+﻿
+namespace VacuumLoggerVIT2.VacuumLogger.Application
+{
+    internal interface IParse
+    {
+        public string ParsedString();
+        public struct ParsedStruct
+        {
+            public string Name;
+            public string Value;
+            public string Type; 
+        }
+    }
+}
---
+++ VacuumLoggerVIT2/VacuumLoggerVIT2/VacuumLogger.Application/Parser.cs
@@ -0,0 +1,49 @@
+﻿
+namespace VacuumLoggerVIT2.VacuumLogger.Application
+{
+    public class Parser : IParse
+    {
+        private string _parsedString;
+        private string _name;
+        private string _value;
+        private string _type;
+        public Parser()
+        {
+            _name = "DefaultName";
+            _value = "DefaultValue";
+            _type = "DefaultString";
+        }
+        public Parser(string name)
+        {
+            _name = name;
+            _value = "DefaultValue";
+            _type = "DefaultString";
+        }
+        public Parser(string name, string value)
+        {
+            _name = name;
+            _value = value;
+            _type = "DefaultString";
+        }
+        public Parser (string name, string value, string type)
+        {
+            _name = name;
+            _value = value;
+            _type = type;
+            var ps = new ParsedStruct();
+            ps.Name = _name;
+            ps.Value = _value;
+            ps.Type = _type;
+        }
+        public string ParsedString()
+        {
+            return _parsedString;
+        }
+        public struct ParsedStruct
+        {
+            public string Name;
+            public string Value;
+            public string Type;
+        }
+    }
+}
